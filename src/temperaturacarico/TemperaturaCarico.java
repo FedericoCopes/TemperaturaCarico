3056fb428b0b5247e0b070119703b86b789a5dee
Mattiaadamoli <mattia.adamoli@mpolo.it>
2023-01-16 16:50:19 +0100

    Modifiche
    Lettura da file XML, verifica della temperatura (chiesta all'utente)

---
+++ src/temperaturacarico/TemperaturaCarico.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package temperaturacarico;
+
+import java.io.IOException;
+import java.util.Scanner;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import static temperaturacarico.VeicoliXML.readXMLFile;
+
+/**
+ *
+ * @author FEDERICOCOPES
+ */
+public class TemperaturaCarico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        Scanner input = new Scanner(System.in);
+        //File fXmlFile = new File(filePath);
+        double threshold;
+        System.out.print("Inserire il valore di soglia con cui si vogliono confrontare le misure rilevate: ");
+        threshold = input.nextDouble();
+        try {
+            readXMLFile(args[0], threshold);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            System.err.println("Errore durante il parsing del file XML: " + e.getMessage());
+        }
+    }
+}
