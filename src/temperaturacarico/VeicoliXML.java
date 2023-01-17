bdf0f86916252e2c19a8de20f9bae981789d93f7
Mattiaadamoli <mattia.adamoli@mpolo.it>
2023-01-17 10:20:42 +0100

    Update VeicoliXML.java

--- src/temperaturacarico/VeicoliXML.java
+++ src/temperaturacarico/VeicoliXML.java
@@ -14,7 +14,7 @@ import org.xml.sax.*;
 
 public class VeicoliXML {
 
-    public static void readXMLFile(String filePath, double threshold) throws ParserConfigurationException, SAXException, IOException {
+    public static void readXMLFile(String filePath, double soglia) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(filePath);
@@ -24,35 +24,29 @@ public class VeicoliXML {
 
         for (int i = 0; i < veicoloList.getLength(); i++) {
             Node veicoloNode = veicoloList.item(i);
-
             if (veicoloNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element veicoloElement = (Element) veicoloNode;
                 String id = veicoloElement.getElementsByTagName("ID").item(0).getTextContent();
                 NodeList misuraList = veicoloElement.getElementsByTagName("misura");
-                boolean allBelowThreshold = true;
-                List<Long> timestampAboveThreshold = new ArrayList<>();
-
+                boolean bool = true;
+                List<Long> TimeStamp = new ArrayList<>();
                 for (int j = 0; j < misuraList.getLength(); j++) {
                     Node misuraNode = misuraList.item(j);
-
                     if (misuraNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element misuraElement = (Element) misuraNode;
                         double temperature = Double.parseDouble(misuraElement.getElementsByTagName("temperatura").item(0).getTextContent());
                         String dataOra = misuraElement.getElementsByTagName("data_ora").item(0).getTextContent();
-
-                        if (temperature > threshold) {
-                            allBelowThreshold = false;
-                            // Utilizzare la classe LocalDateTime o SimpleDateFormat per convertire la stringa dataOra in un oggetto Timestamp
+                        if (temperature > soglia) {
+                            bool = false;
                             LocalDateTime date = LocalDateTime.parse(dataOra);
                             long timestamp = date.toEpochSecond(ZoneOffset.UTC);
-                            timestampAboveThreshold.add(timestamp);
+                            TimeStamp.add(timestamp);
                         }
                     }
                 }
-
-                System.out.println("Il veicolo con ID: " + id + " ha tutte le temperature sotto la soglia? : " + allBelowThreshold);
-                if (!allBelowThreshold) {
-                    System.out.println("Timestamp in cui la temperatura e' superiore al valore di soglia : " + timestampAboveThreshold);
+                System.out.println("Il veicolo con ID: " + id + " ha tutte le temperature sotto la soglia? : " + bool);
+                if (!bool) {
+                    System.out.println("Timestamp in cui la temperatura e' superiore al valore di soglia : " + TimeStamp);
                 }
             }
         }
